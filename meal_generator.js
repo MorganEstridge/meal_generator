060bad67f8d55d1cfed35e8584313f793f3aef99
Morgan Estridge <morganjoelle9799@gmail.com>
2021-09-07 09:59:18 -0400

    Add getters and setter methods

--- meal_generator.js
+++ meal_generator.js
@@ -1,1 +1,19 @@
-console.log('yo')
+// Object that creates and stores a random meal
+const mealGenerator = {
+    _meal: {
+        types: ['breakfast', 'brunch', 'lunch', 'dinner'], 
+        locations: [], // name of restaurant or at home 
+        dishes: []  // food options
+    },
+
+    // getters and setters
+    get types() { 
+        return this._meal.types; 
+    },
+    get locations() { return this._meal.locations; },
+    get dishes() { return this._meal.dishes; },
+    set types(typesArr) { this._meal.types = typesArr; },
+    set locations(locationsArr) { this._meal.locations = locationsArr; },
+    set dishes(dishesArr) { this._meal.dishes = dishesArr; },
+};
+
