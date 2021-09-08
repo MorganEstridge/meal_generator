d064f0d684af49809004c5ea77b0cd9632c5ec19
Morgan Estridge <morganjoelle9799@gmail.com>
2021-09-08 15:32:53 -0400

    Add getRandom and addItem methods

--- meal_generator.js
+++ meal_generator.js
@@ -15,5 +15,31 @@ const mealGenerator = {
     set types(typesArr) { this._meal.types = typesArr; },
     set locations(locationsArr) { this._meal.locations = locationsArr; },
     set dishes(dishesArr) { this._meal.dishes = dishesArr; },
+
+    //Return random items in each _meal array
+    getRandomType() {
+        const randNum = Math.floor(Math.random() * this._meal.types.length);
+        return this._meal.types[randNum]; 
+    },
+    getRandomLocation() {
+        const randNum = Math.floor(Math.random() * this._meal.locations.length);
+        return this._meal.locations[randNum]; 
+    },
+    getRandomDish() {
+        const randNum = Math.floor(Math.random() * this._meal.dishes.length);
+        return this._meal.dishes[randNum]; 
+    },
+
+    //Add new items to arrays
+    addType(type) {
+        this._meal.types.push(type);
+    },
+    addLocation(location) {
+        this._meal.locations.push(location);
+    },
+    addDish(dish) {
+        this._meal.dishes.push(dish);
+    }
+
 };
 
